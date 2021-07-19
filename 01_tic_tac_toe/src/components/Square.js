85cb0f568542133202f55c7b317877ea85462745
leyuri <leyuri97@gmail.com>
2021-07-19 21:50:32 +0900

    [01_tic_tac_toe] Add : Square 에 props value 추가

--- 01_tic_tac_toe/src/components/Square.js
+++ 01_tic_tac_toe/src/components/Square.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 class Square extends React.Component {
+    // this.props = {value: "1"}
     render() { //jsx
         return (
-            <button className="square">Square</button>
+            <button className="square">{this.props.value}</button>
         );
     }
 }
