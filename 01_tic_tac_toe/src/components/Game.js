21aa00301eb3e68a5c564c87a88abe8fcfb88ee3
leyuri <leyuri97@gmail.com>
2021-07-21 11:58:00 +0900

    [01_tic_tac_toe] Add : nextPlay function 추가(xIsNext 대신)

--- 01_tic_tac_toe/src/components/Game.js
+++ 01_tic_tac_toe/src/components/Game.js
@@ -9,7 +9,6 @@ class Game extends Component {
                 squares: Array(9).fill(null),
             }],
             step: 0,
-            xIsNext: true,
         };
     }
 
@@ -32,26 +31,28 @@ class Game extends Component {
         return null; // 다 돌았는데도 아무도 찾지 못했으면 이긴 사람이 없다는 뜻, null 값을 리턴
     }
 
+    nextPlayer(idx) {
+        return (idx % 2 === 0) ? "X": "O";
+    }
+
     handleClick(i) {
-        const { history, xIsNext, step } = this.state;
+        const { step } = this.state;
+        const history = this.state.history.slice(0, this.state.step + 1)
         const current = history[step];
         const squares = current.squares.slice(); // copy
         if (squares[i] !== null || this.calculateWinner(squares)) { // arr[i]
             return
         }
-        squares[i] = xIsNext ? "X" : "O";
+        squares[i] = this.nextPlayer(step)
         this.setState({
             history: history.concat([{ squares: squares }]),
             step: step + 1,
-            xIsNext: !xIsNext
         })
     }
 
     goto(idx) {
         this.setState({
-            // history: this.state.history,
             step: idx,
-            xIsNext: (idx % 2 === 0) ? "X": "O"
         })
     }
 
@@ -61,10 +62,8 @@ class Game extends Component {
             return (
                 <li>
                     <button onClick={
-                        () => {this.getSnapshotBeforeUpdate(idx);}
-                    }>
-                        {desc}
-                    </button>
+                        () => {this.goto(idx);}
+                    }>{desc}</button>
                 </li>
 
             )
@@ -72,14 +71,14 @@ class Game extends Component {
     }
 
     render() {
-        const { history, xIsNext } = this.state;
+        const { history, step } = this.state;
         const squares = history[this.state.step].squares;
         const winner = this.calculateWinner(squares);
         let status; // status 값은 변경 가능해야 하므로 let keyword 사용
         if (winner) { // winner가 있으면    
             status = `Winner: ${winner}`;
         } else {
-            status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+            status = `Next player: ${this.nextPlayer(step)}`;
         }
         return (
             <div className="game">
