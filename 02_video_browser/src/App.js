00c874110c633daecd5abadb8a932a54531de44a
leyuri <leyuri97@gmail.com>
2021-07-21 16:24:53 +0900

    [02_video_browser] Add : state 추가(class 기반 컴포넌트로 수정)

--- 02_video_browser/src/App.js
+++ 02_video_browser/src/App.js
@@ -4,29 +4,41 @@ import * as search from 'youtube-search';
 
 import SearchBar from './components/search_bar'
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: [],
+      selectedVideo: null
+    };
+    var opts = {
+      maxResults: 20,
+      key: 'AIzaSyC8ew0DgXP36CquBiAw75B7hWRsfvRxhDs'
+    };
 
-  var opts = {
-    maxResults: 20,
-    key: 'AIzaSyC8ew0DgXP36CquBiAw75B7hWRsfvRxhDs'
-  };
+    search('떡볶이', opts, (err, results) => {
+      if (err) return console.log(err);
+      this.setState({
+        video: results,
+        selectedVideo: results[0]
+      })
+      console.dir(results);
+    });
+  }
 
-  search('떡볶이', opts, function (err, results) {
-    if (err) return console.log(err);
-    console.dir(results);
-  });
-
-  return (
-    <div className="App container mt-3">
-      <SearchBar />
-      <div className="row">
-        <div className="col-8">
-        </div>
-        <div className="col-4">
+  render() {
+    return (
+      <div className="App container mt-3" >
+        <SearchBar />
+        <div className="row">
+          <div className="col-8">
+          </div>
+          <div className="col-4">
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
