921932c512df42bd9beaf7b047ca6036b5cf5f6b
leyuri <leyuri97@gmail.com>
2021-07-21 15:20:11 +0900

    [02_video_browser] Add : youtube-search library 추가

--- 02_video_browser/src/App.js
+++ 02_video_browser/src/App.js
@@ -1,21 +1,30 @@
+import React from 'react'
 import './App.css';
+import * as search from 'youtube-search';
 
 import SearchBar from './components/search_bar'
 
 function App() {
+
+  var opts = {
+    maxResults: 20,
+    key: 'AIzaSyC8ew0DgXP36CquBiAw75B7hWRsfvRxhDs'
+  };
+
+  search('떡볶이', opts, function (err, results) {
+    if (err) return console.log(err);
+    console.dir(results);
+  });
+
   return (
     <div className="App container mt-3">
-      
       <SearchBar />
       <div className="row">
         <div className="col-8">
-
         </div>
         <div className="col-4">
-
         </div>
       </div>
-
     </div>
   );
 }
