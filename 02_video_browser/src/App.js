d6dfdd1fcb3336991c325a6eb708d9bf6e912019
leyuri <leyuri97@gmail.com>
2021-07-21 15:03:26 +0900

    [02_video_browser] Add : Component 추가

--- 02_video_browser/src/App.js
+++ 02_video_browser/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+import SearchBar from './components/search_bar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-3">
+      
+      <SearchBar />
+      <div className="row">
+        <div className="col-8">
+
+        </div>
+        <div className="col-4">
+
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ 02_video_browser/src/components/search_bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+    render() {
+        return (
+            <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+        )
+    }
+}
+
+export default SearchBar;
